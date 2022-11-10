9db5db8b040be4660e812e68a5a52b4b95abe9bf
Nathan-Matthews <nate.matthews24@gmail.com>
2022-11-10 11:59:38 -0500

    Div being deleted after being added, reason unknown

--- lib.js
+++ lib.js
@@ -1,25 +1,20 @@
 let myLibrary = [];
 
-function Book(title, author, pages, readBool){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readBool = readBool;
-  this.index;
-  this.info = function() {
-    return title + ' by ' + author + ' , ' + pages + ' , ' + readBool;
+class Book {
+  constructor(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    //this.readBool = readBool;
+    // this.info = function () {
+    //   return title + ' by ' + author + ' , ' + pages + ' , ' + readBool;
+    // };
   }
 }
 
 // When 'Add book' is clicked, retrieved the form inputs.
 const addBook = document.querySelector(".add");
 addBook.addEventListener("click", createNewBookObject);
-/*
-  able to get form info in 'createNewBookObject' but
-  it is not being added to myLibrary properly.
-
-  Also pages is being read as a string.
-*/
 
 function removeAllChildNodes(parent) {
   while(parent.firstChild){
@@ -33,50 +28,27 @@ function createNewBookObject() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
-  let readBool = document.getElementById("readBool").value;
+  //let readBool = document.getElementById("readBool").value;
 
-  const book = new Book(title, author, pages, readBool);
-
-  book.index = myLibrary.length;  
+  let book = new Book(title, author, pages);
+  console.log(book);
   myLibrary.push(book);
-
-  displayLibrary(myLibrary);
-
-  return;
+  displayLibrary();
 }
 
 // Selector to target library container
 const lib = document.querySelector(".library");
-function displayLibrary(myLibrary) {
-  //removeAllChildNodes(lib);
+function displayLibrary() {
+  removeAllChildNodes(lib);
   for(i = 0; i < myLibrary.length; i++){
-    console.log(myLibrary[i]);
-
     // Create a new div to append to the library container
-
-    //const divContainer = document.createElement("div");
     const divTitle = document.createElement("div");
-    //const divAuthor = document.createElement("div");
-    //const divPages = document.createElement("div");
 
     // Add the title of the book to the text content of div
-
     divTitle.textContent = myLibrary[i].title;
-    //divAuthor.textContent = myLibrary[i].author;
-    //divPages.textContent = myLibrary[i].pages;
 
     // Append the div to the library container
     document.querySelector(".library").appendChild(divTitle);
   }
 }
-
-// const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-// const deepWork = new Book("Deep Work", "Cal N.", 260, true);
-// const theMartian = new Book("The Martian", "Andy W.", 300, true);
-// const theOneThing = new Book("The One Thing", "Gary S.", 168, false);
-// addBookToLibrary(theHobbit);
-// addBookToLibrary(deepWork);
-// addBookToLibrary(theMartian);
-// addBookToLibrary(theOneThing);
-
 
