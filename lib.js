883768b15f172d1f23d096ac090903c49461b3d6
Nathan-Matthews <nate.matthews24@gmail.com>
2022-11-11 14:50:20 -0500

    Add basic styling

--- lib.js
+++ lib.js
@@ -2,14 +2,14 @@
 let myLibrary = [];
 
 // Constructor for Book object
-function Book(title, author, pages, readBool){
+function Book(title, author, rating, readBool){
   this.title = title
   this.author = author
-  this.pages = pages
+  this.rating = rating
   this.readBool = readBool
   this.index
   this.info = function() {
-    return title + ' by ' + author + ' , ' + pages + ' , ' + readBool;
+    return title + ' by ' + author + ' , ' + rating + ' , ' + readBool;
   }
 }
 
@@ -19,19 +19,11 @@ addBook.addEventListener("click", function(){
   displayLibrary(true);
 }, false);
 
-document.querySelectorAll('.delete').forEach(item => {
-  item.addEventListener('click', event => {
-    console.log("You made it!");
-  })
-})
-
 // Deletes a book from myLibrary
 function removeBookFromLibrary(name) {
-  console.log(name.slice(-1))
   myLibrary.splice(parseInt(name.slice(-1)),1);
   updateLibraryIndex();
   displayLibrary(false);
-  console.log(myLibrary);
 }
 
 // Updates the Library index after a book is deleted.
@@ -56,10 +48,10 @@ function removeAllChildNodes(parent) {
 function createNewBookObject() {
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
-  let pages = document.getElementById("pages").value;
+  let rating = document.getElementById("rating").value;
   let readBool = document.getElementById("readBool").checked;
 
-  const book = new Book(title, author, pages, readBool);  
+  const book = new Book(title, author, rating, readBool);  
   addBookToLibrary(book);
 }
 // Adds a book to the library array.
@@ -79,38 +71,38 @@ function displayLibrary(addNewBookBool) {
     createNewBookObject();
   }
   for(i = 0; i < myLibrary.length; i++){
-    console.log(myLibrary[i]);
-
     // Create a new div to append to the library container
     const divContainer = document.createElement("div");
     const divButton = document.createElement("button");
     const divTitle = document.createElement("div");
     const divAuthor = document.createElement("div");
-    const divPages = document.createElement("div");
-    const divReadBool = document.createElement("div");
+    const divRating = document.createElement("div");
+    const divReadBool = document.createElement("checkbox");
 
     // Add the content of the book to the textcontent
-    divButton.textContent = "Delete";
+    divButton.textContent = "X";
     divButton.className = "delete ";
     divButton.className += i;
     divTitle.textContent = myLibrary[i].title;
     divAuthor.textContent = myLibrary[i].author;
-    divPages.textContent = myLibrary[i].pages;
+    divRating.textContent = myLibrary[i].rating;
 
     // Checks the checkbox to see if the book has been read yet.
     if(myLibrary[i].readBool){
-      divReadBool.textContent = "You have read this book.";
+      divReadBool.checked = true;
     }
     else{
-      divReadBool.textContent = "You have NOT read this book yet!";
+      divReadBool.checked = false;
     }
+    divReadBool.textContent = "Read Status:";
 
-    // Append the book container to the library container and then the info to the book container.
+    // Append the book container to the library container and then the book info to the book container.
     document.querySelector(".library").appendChild(divContainer);
+    divContainer.className = "card";
     divContainer.appendChild(divButton);
     divContainer.appendChild(divTitle);
     divContainer.appendChild(divAuthor);
-    divContainer.appendChild(divPages);
+    divContainer.appendChild(divRating);
     divContainer.appendChild(divReadBool);
 
   }
@@ -125,14 +117,15 @@ function displayLibrary(addNewBookBool) {
 }
 
 
-// Example Books
-const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, false);
-const deepWork = new Book("Deep Work", "Cal N.", 260, true);
-const theMartian = new Book("The Martian", "Andy W.", 300, true);
-const theOneThing = new Book("The One Thing", "Gary S.", 168, false);
+// Example Books included on initial page load.
+const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 5, false);
+const deepWork = new Book("Deep Work", "Cal N.", 5, true);
+const theMartian = new Book("The Martian", "Andy W.", 4, true);
+const theOneThing = new Book("The One Thing", "Gary S.", 4, false);
 addBookToLibrary(theHobbit);
 addBookToLibrary(deepWork);
 addBookToLibrary(theMartian);
 addBookToLibrary(theOneThing);
 
+// Run the displayLibrary once to show example books on page load.
 displayLibrary(false);
